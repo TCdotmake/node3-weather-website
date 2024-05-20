5219282994bc1078b62a55418c3b6b820ece71aa
Tommy Chen <tommyjchen@hotmail.com>
2024-05-20 20:20:17 +0100

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,32 +10,21 @@ weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   forecast.innerHTML = "";
   errorMsg.innerHTML = "Loading...";
-  fetch(`http://localhost:3000/weather?address=${addressInput.value}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          errorMsg.innerHTML = data.error;
-          forecast.innerHTML = null;
-          return;
-        }
-        errorMsg.innerHTML = null;
-        forecast.innerHTML = `
+
+  fetch(`/weather?address=${addressInput.value}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        errorMsg.innerHTML = data.error;
+        forecast.innerHTML = null;
+        return;
+      }
+      errorMsg.innerHTML = null;
+      forecast.innerHTML = `
         <p>Location: ${data.location}</p>
         <p>Latitude: ${data.latitude}  Longitude: ${data.longitude}</p>
         <p>Temp(C): ${data.temp}</p>
         <p>Condition: ${data.condition}</p>
         `;
-      });
-    }
-  );
-});
-
-/* fetch("http://localhost:3000/weather?address=boston").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      return console.log(data.error);
-    }
-    console.log(data);
+    });
   });
 });
- */
